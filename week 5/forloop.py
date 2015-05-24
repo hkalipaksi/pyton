30b8308d16c8fae52722824e452bf1b163fabb39
hkalipaksi <hkalipaksi@gmail.com>
2015-05-24 11:33:52 +0700

    initial commit
    week 5

---
+++ week 5/forloop.py
@@ -0,0 +1,16 @@
+#print 1 2 3 4 5
+print("# print 1 2 3 4 5")
+for loop in range (1,6):
+  print loop,
+print "\n"
+
+#print 0 2 4 6 8
+print("# print 0 2 4 6 8")
+for loop in range (0,10,2):
+  print loop,
+print "\n"
+
+#print 5 4 3 2 1
+print("# print 5 4 3 2 1 ")
+for loop in range (5,0,-1):
+  print loop,
