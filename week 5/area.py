034c8e28d2de34c9fa93606d5ecc088ecf80af14
hkalipaksi <hkalipaksi@gmail.com>
2015-05-24 11:15:31 +0700

    Revert "initial commit"
    This reverts commit 595a0a677099aa3e2b14247f92e9acafcd4aafab.

--- week 5/area.py
+++ week 5/area.py
@@ -8,21 +8,18 @@ while close != "yes":
   choice=input("your choice:")
   if choice==1:
     print("choice is 1")
-    print("program to calculate area of triangle")
     base=input("input base:")
     height=input("input height:")
     areatriangle=(base*height)/2
     print("area of triangle:{0}".format(areatriangle))
     print("finish")
   elif choice==2:
-    print("program to calculate area of square")
     print("choice is 2")
     side=input("input side:")
     areasquare=side*side
     print("area of square:{0}".format(areasquare))
     print("finish")
   elif choice==3:
-    print("program to calculate area of rectangle")
     print("choice is 3")
     width=input("input widht:")
     height=input("input height:")
